99d41c3d957ed6271fd9911c290e5ee3ac653000
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-01 12:34:42 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,16 @@ function App() {
       <div id="popups" className="mt-3">
         <div className="poppup-card">
           <div className="popup-headung">
-            {" "}
             <span className="ortho popup-head">
-              {" "}
-              ৫ ওয়াক্ত ফরজ নামাজের পর সবচেয়ে উত্তম কাজ হলো: তাজবিহ পাঠ করা{" "}
+              ৫ ওয়াক্ত ফরজ নামাজের পর সবচেয়ে উত্তম কাজ হলো: তাজবিহ পাঠ করা
             </span>
             <div className="popup-text">
-              {" "}
               <span className="ortho popup-text-content">
-                {" "}
                 রাসূলুল্লাহ (সা.) ইরশাদ করেছেন, ‘যে ব্যক্তি প্রত্যেক নামাজের পর
                 ৩৩ বার সুবহানাল্লাহ, ৩৩ বার আলহামদুলিল্লাহ এবং ৩৩ বার আল্লাহু
                 আকবার পাঠ করবে, আল্লাহ তার সব পাপ ক্ষমা করে দেবেন, যদিও গোনাহ
-                সমুদ্রের ফেনা পরিমাণ হয়।’ (সহিহ মুসলিম)।{" "}
-              </span>{" "}
+                সমুদ্রের ফেনা পরিমাণ হয়।’ (সহিহ মুসলিম)।
+              </span>
               {/* <div className="d-flex">
                 <button className="popup-colse-button"
                 //  onclick="closePopup()"
